d89ca3b88e510099ba4eb32e3ee71181db374439
dragon <rongjihuang@gmail.com>
2015-02-12 18:32:38 +0800

    temp

---
+++ simter-sso-server/src/main/java/org/simter/sso/server/BaseServlet.java
@@ -0,0 +1,11 @@
+package org.simter.sso.server;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * 整合 Spring 的基础 Servlet
+ * Created by dragon on 2015/2/12.
+ */
+public class BaseServlet extends HttpServlet {
+
+}
